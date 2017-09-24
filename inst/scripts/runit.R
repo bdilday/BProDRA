27f13689ffe2b568d15bfcb436f355cac85127f0
Ben Dilday <ben.dilday.phd@gmail.com>
2017-09-23 22:33:33 -0400

    stan script

---
+++ inst/scripts/runit.R
@@ -0,0 +1,12 @@
+
+
+run_it <- function() {
+  library(rstan)
+  model_df <-  BProDRA::generate_model_df(year=2016)
+  mods <- BProDRA::initialize_with_lme4(model_df$ev)
+  model_df <- BProDRA::update_ans(model_df, mods)
+  options(mc.cores = parallel::detectCores())
+  rstan_options(auto_write = TRUE)
+  do_stan_fit()
+
+}
